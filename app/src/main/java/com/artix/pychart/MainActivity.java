3712e2652664df20a47da27a6393bfd9f0e909d0
somnath <somnathnath8482@gmail.com>
2022-10-30 14:53:08 +0530

    smn

--- app/src/main/java/com/artix/pychart/MainActivity.java
+++ app/src/main/java/com/artix/pychart/MainActivity.java
@@ -23,20 +23,16 @@ PiChart py_ch;
 
 
         List<PiData> piData = new ArrayList<>();
-        piData.add(new PiData(1,"1",13));
-        piData.add(new PiData(2,"2",73));
-        piData.add(new PiData(3,"3",13));
-        piData.add(new PiData(4,"4",19));
-        piData.add(new PiData(5,"5",11));
-        piData.add(new PiData(6,"6",25));
-        piData.add(new PiData(7,"7",12));
-        piData.add(new PiData(1,"8",43));
-        piData.add(new PiData(2,"9",22));
-        piData.add(new PiData(3,"10",33));
-        piData.add(new PiData(3,"11",77));
-        piData.add(new PiData(3,"12",67));
-        piData.add(new PiData(3,"13",56));
-        piData.add(new PiData(3,"14",31));
+        piData.add(new PiData(1,"Goa 1-40",13));
+        piData.add(new PiData(2,"Kolkatah 80-40",73));
+        piData.add(new PiData(3,"Koar - 60-40",13));
+        piData.add(new PiData(4,"sanghai 70-40",19));
+        piData.add(new PiData(5,"Trichi 40-46",11));
+        piData.add(new PiData(6,"samon 55-40",25));
+        piData.add(new PiData(7,"bisgur 55-65",12));
+        piData.add(new PiData(1,"Ohio 44-12",43));
+        piData.add(new PiData(2,"Tala 77-34",22));
+        piData.add(new PiData(3,"Ertop 66-89",33));
         py_ch.setPiData(piData);
     }
 }
